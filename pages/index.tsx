bfbe3f3003e643d4c3731b4a8d054d9c9743b4b5
Hyeon1445 <lily1445@gmail.com>
2021-12-14 21:20:32 +0900

    chore: install material ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Button from '@mui/material/Button'
 
 const Home: NextPage = () => {
   return (
     <div className="font-bold bg-red-100">
-      hello
+      <p>hello</p>
+      <Button variant="contained">World</Button>
     </div>
   )
 }
