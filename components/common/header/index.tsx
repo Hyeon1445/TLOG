5c438ac460793548eb1fca83a96f5844169f9a3c
Hyeon1445 <lily1445@gmail.com>
2022-01-04 23:25:51 +0900

    feat: create BaseHeader component

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
 
-const BaseHeader = () => {
+type HeaderProps = {
+  title: string
+}
+
+const BaseHeader = ({ title }: HeaderProps) => {
 
   return (
-    <header>purple header</header>
+    <header className="header h-10 bg-violet-300 text-white flex justify-center items-center">
+      <MenuIcon
+        className="absolute left-4"
+      />
+      {title}
+    </header>
+    // TODO: NavMenu (Desktop) / sidemenu(PC)
   )
 }
 
